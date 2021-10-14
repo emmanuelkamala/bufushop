49bff4338ff77c981b1ca7ff957bf1bb25615dc9
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-10-15 00:52:36 +0300

    Edit order model

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -7,7 +7,10 @@ const OrderSchema = new mongoose.Schema({
       productId: { type: String },
       quantity: { type: Number, default: 1 }
     }
-  ]
+  ],
+  amount: { type: Number, required: true },
+  address: { type: Object, required: true },
+  isDelivered: { type: String, default: "pending" }
 }, 
   { timestamps: true }
 )
